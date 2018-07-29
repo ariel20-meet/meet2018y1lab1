4a818f9b52deee33da6ad0f854c1276884d4570f
ariel20-meet <ariel20@meet.mit.edu>
2018-07-29 09:18:10 -0400

    Stopping here

---
+++ meetinturtle.py
@@ -0,0 +1,141 @@
+import turtle
+
+# Everything that comes after the # is a 
+# comment.
+# It is a note to the person reading the code.
+# The computer ignores it.
+# Write your code below here...
+
+
+# ...and end it before the next line.
+
+
+turtle.penup()     #Brings the pen up, so
+                #nothing will be drawn
+turtle.pendown()    #Puts the pen down, so we
+                #are ready to draw!
+turtle.goto(0,0) #Go to the position “x"&"y", 
+#but write in numbers 
+#instead
+turtle.penup() #Pick up the pen so it doesn’t 
+               #draw
+turtle.goto(-200,-100) #Move the turtle to the 
+ #position (-200, -100) 
+ #on the screen
+turtle.pendown() #Put the pen down to start
+                 #drawing
+#Draw the M:
+turtle.goto(-200,-100+200) 
+turtle.goto(-200+50,-100) 
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100) 
+
+# draw first E
+turtle.penup()
+
+turtle.goto(-200+150,-100) 
+
+turtle.pendown()
+
+turtle.goto(-200+150,100)
+
+turtle.goto(-200+250,100) 
+
+turtle.penup()
+
+turtle.goto(-200+150,100)
+
+turtle.goto(-200+150,10)
+
+turtle.pendown()
+
+turtle.goto(-200+250,10)
+
+turtle.penup()
+
+turtle.goto(-200+150,100)
+
+
+turtle.goto(-200+150,-100)
+
+
+turtle.pendown()
+# draw second E
+
+turtle.penup()
+
+turtle.goto(-200+250+50,-100) 
+
+turtle.pendown()
+
+turtle.goto(-200+250+50,100)
+
+turtle.goto(-200+350+50,100) 
+
+turtle.penup()
+
+turtle.goto(-200+250+50,100)
+
+turtle.goto(-200+250+50,10)
+
+turtle.pendown()
+
+turtle.goto(-200+350+50,10)
+
+turtle.penup()
+
+turtle.goto(-200+250+50,100)
+
+turtle.goto(-200+250+50,-100)
+
+turtle.goto(-200+350+50,-100)
+
+turtle.pendown()
+
+turtle.goto(-200+350+20-20-30-10-7,-100)
+
+turtle.penup()
+
+
+turtle.goto(-200+350+20-20-30-10-7-50-100,-100)
+
+turtle.pendown()
+
+turtle.goto(-200+350+20-20-30-10-7-50-100+70+50,-100)
+
+
+
+turtle.penup()
+
+
+turtle.goto(-200+350+20-20-30-10-7-50-100+70+50+220,-100)
+
+
+turtle.pendown()
+
+turtle.goto(-200+350+20-20-30-10-7-50-100+70+50+220,-100+100+200-40-20-20-10)
+
+
+turtle.penup()
+
+
+turtle.goto(-200+350+20-20-30-10-7-50-100+220+50,-100+100+200-40-20-20-10)
+
+
+turtle.pendown()
+
+
+turtle.goto(-200+350+20-20-30-10-7-50-100+220+50+150,-100+100+200-40-20-20-10)
+
+
+
+
+
+
+
+
+
+
+turtle.mainloop()
+
+
